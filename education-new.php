46582080b01b76de3d1f1456e4d7473233d667dd
Kyle Marcy <kyle@hencove.com>
2024-09-19 15:24:03 -0400

    initial commit

---
+++ education-new.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Template for search results.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+<?php
+/**
+ * Header template.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+<!DOCTYPE html>
+<html class="<?php avada_the_html_class(); ?>" <?php language_attributes(); ?>>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<?php Avada()->head->the_viewport(); ?>
+
+	<?php wp_head(); ?>
+
+	<?php
+	/**
+	 * The setting below is not sanitized.
+	 * In order to be able to take advantage of this,
+	 * a user would have to gain access to the database
+	 * in which case this is the least of your worries.
+	 */
+	echo apply_filters( 'avada_space_head', Avada()->settings->get( 'space_head' ) ); // phpcs:ignore WordPress.Security.EscapeOutput
+	?>
+</head>
+
+<?php
+$object_id      = get_queried_object_id();
+$c_page_id      = Avada()->fusion_library->get_page_id();
+$wrapper_class  = 'fusion-wrapper';
+$wrapper_class .= ( is_page_template( 'blank.php' ) ) ? ' wrapper_blank' : '';
+?>
+<body <?php body_class(); ?> <?php fusion_element_attributes( 'body' ); ?>>
+	<?php do_action( 'avada_before_body_content' ); ?>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'Avada' ); ?></a>
+
+	<div id="boxed-wrapper">
+		<?php if ( 'boxed' === Avada()->settings->get( 'layout' ) ) : ?>
+			<?php if ( 'framed' === Avada()->settings->get( 'scroll_offset' ) ) : ?>
+				<div class="fusion-sides-frame"></div>
+			<?php endif; ?>
+		<?php endif; // End of boxed mode check. ?>
+
+		<div id="wrapper" class="<?php echo esc_attr( $wrapper_class ); ?>">
+			<div id="home" style="position:relative;top:-1px;"></div>
+			<?php if ( apply_filters( 'awb_should_render_header', true, $c_page_id ) ) : ?>
+				<?php if ( has_action( 'avada_render_header' ) ) : ?>
+					<?php do_action( 'avada_render_header' ); ?>
+				<?php else : ?>
+
+					<?php avada_header_template( 'below', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+					<?php if ( 'left' === fusion_get_option( 'header_position' ) || 'right' === fusion_get_option( 'header_position' ) ) : ?>
+						<?php avada_side_header(); ?>
+					<?php endif; ?>
+
+					<?php avada_sliders_container(); ?>
+
+					<?php avada_header_template( 'above', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+
+				<?php endif; ?>
+			<?php endif; ?>
+
+			<div class="avada-page-titlebar-wrapper">
+				<div class="fusion-page-title-bar education-header fusion-page-title-bar-breadcrumbs fusion-page-title-bar-center">
+					<div class="fusion-page-title-row">
+						<div class="fusion-page-title-wrapper">
+							<div class="fusion-page-title-captions">
+			
+																										<h1 class="darkhone entry-title fusion-responsive-typography-calculated" data-fontsize="46" data-lineheight="normal" style="--fontSize:46;">Education</h1>
+								<p>Use the options below to filter through education resources.</p>
+				<div class="filters">	<?php echo do_shortcode( '[searchandfilter id="555"]' ); ?></div>
+			
+																			
+							</div>
+			
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+
+			<?php
+			$row_css    = '';
+			$main_class = '';
+
+			if ( apply_filters( 'fusion_is_hundred_percent_template', false, $c_page_id ) ) {
+				$row_css    = 'max-width:100%;';
+				$main_class = 'width-100';
+			}
+
+			if ( fusion_get_option( 'content_bg_full' ) && 'no' !== fusion_get_option( 'content_bg_full' ) ) {
+				$main_class .= ' full-bg';
+			}
+			do_action( 'avada_before_main_container' );
+			?>
+			<main id="main" class="clearfix <?php echo esc_attr( $main_class ); ?>">
+				<div class="fusion-row" style="<?php echo esc_attr( $row_css ); ?>">
+
+
+
+<section id="content" class="<?php echo esc_attr( apply_filters( 'awb_content_tag_class', '' ) ); ?>" style="<?php echo esc_attr( apply_filters( 'awb_content_tag_style', '' ) ); ?>">
+	<?php if ( have_posts() ) : ?>
+		
+		<h2 style="color: #4a4a57; text-align: left; font-size: 30px; margin-top: 50px;">
+				<?php
+			//Get a multiple fields values by passing an array of field names
+			//replace `1526` with the ID of your search form
+			global $searchandfilter;
+			$sf_current_query = $searchandfilter->get(555)->current_query();
+		
+			
+			
+			/*
+			 * EXAMPLE 3 - without labels for fields themselves (the `str` value has been changed to include only values)
+			 */
+			$args = array(
+				"str" 					=> '%2$s', 
+				"delim" 				=> array(", ", " - "), 
+				"field_delim"				=> ', ', 
+				"show_all_if_empty"			=> false,
+			);
+			
+			echo $sf_current_query->get_fields_html(
+				array("_sft_category", "_sft_products", "_sft_care-settings"), 
+				$args
+			);
+		
+		?></h2>
+
+		<?php if ( 'bottom' === Avada()->settings->get( 'search_new_search_position' ) ) : ?>
+			<?php get_template_part( 'templates/blog', 'layout' ); ?>
+			<div class="fusion-clearfix"></div>
+		<?php endif; ?>
+
+		<?php if ( 'hidden' !== Avada()->settings->get( 'search_new_search_position' ) ) : ?>
+			<div class="search-page-search-form search-page-search-form-<?php echo esc_attr( Avada()->settings->get( 'search_new_search_position' ) ); ?>">
+				<?php
+				/**
+				 * Render the post title
+				 */
+				 
+				 
+				$title_size = ( false === avada_is_page_title_bar_enabled( 0 ) ? '1' : '2' );
+				//echo avada_render_post_title( 0, false, esc_html__( '', '' ), $title_size ); // phpcs:ignore WordPress.Security.EscapeOutput
+				?>
+				
+			</div>
+		<?php endif; ?>
+
+		<?php if ( 'top' === Avada()->settings->get( 'search_new_search_position' ) || 'hidden' === Avada()->settings->get( 'search_new_search_position' ) ) : ?>
+			<?php get_template_part( 'templates/blog', 'layout' ); ?>
+		<?php endif; ?>
+
+	<?php else : ?>
+
+		<div class="post-content">
+
+			<?php Avada()->template->title_template( esc_html__( 'Couldn\'t find what you\'re looking for!', 'Avada' ) ); ?>
+			<div class="error-page">
+									<div class="filters">	<?php echo do_shortcode( '[searchandfilter id="555"]' ); ?></div>
+			
+										<h3>There are no results for <?php
+				//Get a multiple fields values by passing an array of field names
+				//replace `1526` with the ID of your search form
+				global $searchandfilter;
+				$sf_current_query = $searchandfilter->get(555)->current_query();
+			
+				
+				
+				/*
+				 * EXAMPLE 3 - without labels for fields themselves (the `str` value has been changed to include only values)
+				 */
+				$args = array(
+					"str" 					=> '%2$s', 
+					"delim" 				=> array(", ", " - "), 
+					"field_delim"				=> ', ', 
+					"show_all_if_empty"			=> false,
+				);
+				
+				echo $sf_current_query->get_fields_html(
+					array("_sft_category", "_sft_products", "_sft_care-settings"), 
+					$args
+				);
+			
+			?> <br>
+											Remove some of the filters and try again.</h3>
+			</div>
+		</div>
+	<?php endif; ?>
+</section>
+<?php do_action( 'avada_after_content' ); ?>
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,245 @@
+<?php
+
+function theme_enqueue_styles() {
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function avada_lang_setup() {
+	$lang = get_stylesheet_directory() . '/languages';
+	load_child_theme_textdomain( 'Avada', $lang );
+}
+add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+
+// Register Custom Taxonomy
+function custom_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Care Settings', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Care Setting', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Care Settings', 'text_domain' ),
+		'all_items'                  => __( 'All Care Settings', 'text_domain' ),
+		'parent_item'                => __( 'Parent Care Setting', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Care Setting:', 'text_domain' ),
+		'new_item_name'              => __( 'New Care Setting', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Care Setting', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Care Setting', 'text_domain' ),
+		'update_item'                => __( 'Update Care Setting', 'text_domain' ),
+		'view_item'                  => __( 'View Care Setting', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate Care Settings with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Care Settings', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args   = array(
+		'labels'            => $labels,
+		'hierarchical'      => false,
+		'public'            => true,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'show_in_nav_menus' => true,
+		'show_tagcloud'     => true,
+	);
+	register_taxonomy( 'care-settings', array( 'post', 'article' ), $args );
+
+}
+// add_action( 'init', 'custom_taxonomy', 10 );
+
+// Register Custom Taxonomy
+function product_category() {
+
+	$labels = array(
+		'name'                       => _x( 'Products', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Product', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Products', 'text_domain' ),
+		'all_items'                  => __( 'All Products', 'text_domain' ),
+		'parent_item'                => __( 'Parent Product', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Product:', 'text_domain' ),
+		'new_item_name'              => __( 'New Product', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Product', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Product', 'text_domain' ),
+		'update_item'                => __( 'Update Product', 'text_domain' ),
+		'view_item'                  => __( 'View Product', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate Products with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Products', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args   = array(
+		'labels'            => $labels,
+		'hierarchical'      => false,
+		'public'            => true,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'show_in_nav_menus' => true,
+		'show_tagcloud'     => true,
+	);
+	register_taxonomy( 'products', array( 'post', 'article' ), $args );
+
+}
+// add_action( 'init', 'product_category', 10 );
+
+
+// Add tags to body class
+ add_filter( 'body_class', 'themeprefix_add_taxonomy_class' );
+ // Add taxonomy terms name to body class
+function themeprefix_add_taxonomy_class( $classes ) {
+	if ( is_singular() ) {
+		global $post;
+		$taxonomy_terms = get_the_terms( $post->ID, 'products' ); // change to your taxonomy
+		if ( $taxonomy_terms ) {
+			foreach ( $taxonomy_terms as $taxonomy_term ) {
+				$classes[] = 'tax_' . $taxonomy_term->slug;
+			}
+		}
+	}
+	return $classes;
+}
+
+add_filter( 'ppwp_customize_password_form', 'customize_pwd_form', 10, 3 );
+function customize_pwd_form( $element, $post_id, $wrong_password_message ) {
+	$label           = 'pwbox-' . ( empty( $post_id ) ? rand() : $post_id );
+	$custom_elements = '<div>
+        <div class="panel panel-default account-panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Enter password to access content.</h3>
+            </div>
+            <div class="panel-body">
+                <div class="center-block text-center">
+                    <div class="unlock-form">' . $wrong_password_message . '
+                        <label class="pass-label" for="' . $label . '">' . __( '' ) . ' </label>
+                        <div>
+                            <input name="post_password" id="' . $label . '" type="password" class="form-control" placeholder="Password" required autofocus />
+                        </div>
+		
+                        <div>
+                            <input type="submit" name="Submit" class=" btn-primary btn-block" value="' . esc_attr__( 'Unlock' ) . '" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>';
+		return $custom_elements;
+}
+
+
+function wpr_add_preload() {
+	echo '<link rel="preload" href="/fileadmin/2020/11/Sentec_Background_Hero_2-2.png" as="image">';
+}
+add_action( 'wp_head', 'wpr_add_preload', 5 );
+
+
+
+/* Remove extra categories */
+add_filter( 'the_category_list', function( $categories ) {
+    if( ! empty( $categories ) && is_array($categories) ) {
+        foreach ($categories as $key => $cat) {
+            if( ! $cat->parent ) {
+                $ob[] = $categories[$key];
+                return $ob;
+            }
+        }
+    }
+
+    return $categories;
+}, 100 );
+
+
+/* Custom package English Print Order Form*/
+function nicu_package_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/09/RF-012667-_-NICU-Brochure-VSign-DIGITAL.pdf" target="_blank">NICU Brochure (RF-012667-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/RF-012338-_-INTL-NICU-TCM-Clinical-Impact-Summary.pdf" target="_blank">NICU Clinical Impact Summary (RF-012338-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/11/Balancing-Brain-Lung-Protection-Whitepaper_RF-013873-_-.pdf" target="_blank">Whitepaper - Balancing Brain and Lung protection in the NICU (RF-013873-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/10/RF-013975-_-NonAdhesiveWrapFlyer-HALF-LETTER-DIGITAL-072423-1.pdf">Non Adhesive Wrap Flyer (RF-013975-_)</a>
+    ';
+
+    return $content;
+}
+add_shortcode('nicu_package', 'nicu_package_custom');
+
+function picu_package_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2024/03/RF-014106-_-PICU-Brochure-EN-DIGITAL-013124.pdf" target="_blank">PICU Brochure (RF-014106-a)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/Clinical-Applications-in-Childrens-Hospital-Impact-Summary-A4-oUS-RF-013576-_-.pdf" target="_blank">Clinical Applications in Children’s Hospital – Clinical Impact Summary (RF-013576-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/NIV-in-the-PICU-Whitepaper-US-Letter_RF-013797-_-.pdf" target="_blank">Whitepaper - NIV in the PICU (RF-014073-_)</a>
+    ';
+    return $content;
+}
+add_shortcode('picu_package', 'picu_package_custom');
+
+function sleep_package_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/RF-012668-a-Sleep-Brochure.pdf" target="_blank">SLEEP Brochure (RF-012668-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/11/RF-011745-a-INTL-Sleep-Lab-Clinical-Impact-Summary.pdf" target="_blank">SLEEP Clinical Impact Summary (RF-011745-a)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/CO2-Monitoring-in-Patients-with-V_Q-Mismatch-Whitepaper_081922-FLAT_RF-013000-_-.pdf" target="_blank">Whitepaper - V/Q Mismatch (RF-013000-_)</a>
+    ';
+    return $content;
+}
+add_shortcode('sleep_package', 'sleep_package_custom');
+
+function new_customers_package_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2024/03/RF-012420-a-Implementation-Workbook-DRAFT.pdf" target="_blank">SDMS Implementation Planning Guide (RF-012420-a)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/HB-011657-_-Bedside-Tips-Flyer_UK.pdf" target="_blank">NICU Bedside Tips Sheet (HB-011657-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/12/Troubleshooting-Tips-Sheet-A5-oUS-DIGITAL-HBQ-184-V1.pdf" target="_blank">Troubleshooting Tips Sheet (HBQ-184-V1)</a>
+    ';
+    return $content;
+}
+add_shortcode('new_customers_package', 'new_customers_package_custom');
+
+/* German */
+
+function nicu_paket_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/RF-013464-_-NICU-Brochure-OxiVenT_DE.pdf" target="_blank">NICU Broschüre-DE (RF-013464-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/11/RF-0121.pdf" target="_blank">NICU Clinical Impact Summary-DE (RF-012570-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/10/RF-0131.pdf" target="_blank">Whitepaper - Balancing Brain and Lung protection in the NICU-DE (RF-013962-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/09/RF-014043-_-NonAdhesiveWrapFlyer-DE-A5-PRINT.pdf" target="_blank">Flyer zum Klettbandapplikator (RF-014043-_)</a>
+    ';
+    return $content;
+}
+add_shortcode('nicu_paket', 'nicu_paket_custom');
+
+function picu_paket_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2024/03/RF-014113-_-PICU-Brochure-DE_DIGITAL-031324.pdf" target="_blank">PICU Broschüre-DE (RF-014113-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/10/RF-0131-1.pdf" target="_blank">Clinical Applications in Children’s Hospital – Clinical Impact Summary-DE (RF-013798-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2024/04/RF-014114-_-NIVinthePICU-Whitepaper-A4_DE_DIGITAL_040924.pdf" target="_blank">Whitepaper - NIV in the PICU-DE (RF-014114-_)</a>
+    ';
+    return $content;
+}
+add_shortcode('picu_paket', 'picu_paket_custom');
+
+function sleep_paket_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/RF-013471-_-Sleep-Brochure_DE.pdf" target="_blank">SLEEP Broschüre-DE (RF-013471-_)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2024/03/RF-0121.pdf" target="_blank">SLEEP Clinical Impact Summary-DE (RF-012575-b)</a><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/10/RF-013467-a-CO2-Monitoring-in-Patients-with-V_Q-Mismatch-Whitepaper-A4-DE-DIGITAL-100523.pdf" target="_blank">Whitepaper - V/Q Mismatch-DE (RF-013467-_)</a>
+    ';
+    return $content;
+}
+add_shortcode('sleep_paket', 'sleep_paket_custom');
+
+function neukunden_paket_custom() {
+    $content = '
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="URL_FUR_SDMS_GUIDE_DE" target="_blank">SDMS Implementation Planning Guide-DE (RF-014111-_)</a><br><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/07/HB-012375-a-Bedside-Tips-Sheet_DE.pdf" target="_blank">NICU Bedside Tips Sheet-DE (HB-012375-a)</a><br><br>
+        <span style="color: #ff6600; font-size: 30px; margin-right: 5px;">•</span><a href="https://www.sentec.com/fileadmin/2023/12/HBQ-185-V1-Troubleshooting-Tips-Sheet-A5-DACH-DIGITAL-112823.pdf" target="_blank">Troubleshooting Tips Sheet-DE (HBQ-185-V1)</a>
+    ';
+    return $content;
+}
+add_shortcode('neukunden_paket', 'neukunden_paket_custom');
+
