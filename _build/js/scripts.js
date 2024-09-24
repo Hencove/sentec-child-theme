894ef6925c22050feb79923d8119d883ec5429c6
Alana-Hencove <alana@hencove.com>
2024-09-24 13:55:30 -0400

    WIP

--- _build/js/scripts.js
+++ _build/js/scripts.js
@@ -7,7 +7,74 @@
   \*******************************/
 /***/ (() => {
 
+//
+//	? code here will execute before the DOM has fully loaded
+//
+(function (document, window, $) {
+  //
+  //	? code here will execute once the page is ready
+  //
+  // ... silence is golden
 
+  // get all the filters on this page...
+  var filters = $("body.post-type-archive-sentec-publications .wpc-filters-widget-select");
+
+  // if we got filters...
+  if (!filters) {
+    return;
+  }
+
+  // for each filter: inject (add html after select element, not inside it) new element for the arrow thing (with class)
+  filters.each(function (index, filterEl) {
+    // config standard select2 instance
+    var select2Options = {
+      minimumResultsForSearch: -1
+    };
+
+    // instantiate select2
+    $(filterEl).select2(select2Options);
+
+    //
+
+    // Create the circle element
+
+    // Create the FontAwesome icon element
+
+    // Append the icon to the circle element
+
+    // Append the new circle element to the parent div
+
+    // add an event listener to this new element
+    // $(circleElement).on("click", function () {
+    // 	// console.log(event, filterEl);
+
+    // 	console.log(filterEl);
+
+    // 	// when that element is clicked; "trigger" a click on the select element
+    // 	//
+    // 	//
+    // 	//
+    // 	$(filterEl).trigger("open");
+    // });
+  });
+
+  // todo: maybe use an object literal once we figure it all out
+  var appendSelectElements = {
+    // properties below
+    circleCSSProps: {},
+    // methods below
+    _init: function _init() {},
+    _doSomething: function _doSomething() {
+      var grabFromOtherScope = appendSelectElements.circleCSSProps;
+    }
+  };
+
+  // appendSelectElements._init();
+
+  //
+  //
+  //
+})(document, window, jQuery);
 
 /***/ }),
 
