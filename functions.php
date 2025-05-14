09ff076ae39d5f5673fddfa1e7bb925ce74da383
Alana <alana@hencove.com>
2025-05-14 11:25:35 -0400

    Added GTM scripts to functions.php
    embedding GTM right into the child theme

--- functions.php
+++ functions.php
@@ -291,3 +291,41 @@ function neukunden_paket_custom()
 	return $content;
 }
 add_shortcode('neukunden_paket', 'neukunden_paket_custom');
+
+function add_gtm_code_body()
+{
+?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5W7XDRF"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<?php
+}
+
+add_action('wp_body_open', 'add_gtm_code_body');
+
+
+function add_gtm_code()
+{
+?>
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-5W7XDRF');
+	</script>
+	<!-- End Google Tag Manager -->
+<?php
+}
+add_action('wp_head', 'add_gtm_code');
