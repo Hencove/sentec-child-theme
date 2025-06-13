aecb45571cafb21084c73411b7c88e765d88e48f
Alana <alana@hencove.com>
2025-06-13 15:37:50 -0400

    accessibility -- adding tabindex , focus, active for hover state

---
+++ _source/js/_flipbox.js
@@ -0,0 +1,11 @@
+(function (document, window, $) {
+  //
+  // 		jQuery is ready as $
+
+  document.querySelectorAll(".sentec-flipbox").forEach((el) => {
+    el.setAttribute("tabindex", "0");
+  });
+
+  //
+  //
+})(document, window, jQuery);
